add256dd91a26d5694ffa1922e63b35781324075
CrimsonUnicorn <96168390+CrimsonUnicorn@users.noreply.github.com>
2026-09-06 17:09:59 +0000

    1.6 organize backend architecture

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,18 +2,15 @@ import express  from "express";
 import 'dotenv/config'
 import cors from 'cors'
 
+import healthRoutes from './routes/healthRoutes.js'
+
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors)
 app.use(express.json())
 
-app.get('/api/health', (_req, res) => {
-  res.json({
-    status: 'ok',
-    message: 'NOVA API is running',
-  })
-})
+app.use('/api/health', healthRoutes)
 
 app.listen(PORT, () => {
   console.log(`NOVA API running on port ${PORT}`)
