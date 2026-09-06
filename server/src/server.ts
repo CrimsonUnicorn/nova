73577f2a457d803606595b88fcd32cdcc2520255
CrimsonUnicorn <96168390+CrimsonUnicorn@users.noreply.github.com>
2026-09-06 18:07:13 +0000

    1.8 configure environment variables

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,11 +7,17 @@ import healthRoutes from './routes/healthRoutes.js'
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors)
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+}))
 app.use(express.json())
 
 app.use('/api/health', healthRoutes)
 
-app.listen(PORT, () => {
+app.get("/", (req, res) => {
+  res.send("NOVA API is working!");
+});
+
+app.listen(Number(process.env.PORT) || 5000, "0.0.0.0" ,() => {
   console.log(`NOVA API running on port ${PORT}`)
 })
