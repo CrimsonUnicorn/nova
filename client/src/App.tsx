272a221e58b666b0f9f7becef133a7989d19cc87
CrimsonUnicorn <96168390+CrimsonUnicorn@users.noreply.github.com>
2026-09-11 03:15:15 +0000

    fix: configure vercel spa routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import AppLayout from './layouts/AppLayout'
 
@@ -15,6 +15,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
